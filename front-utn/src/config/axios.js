446aea34a10fb67792b8b2298bde344c477e32e8
Carlos Michael Alexander Barboza <114241344+CarlosMBarboza@users.noreply.github.com>
2025-02-10 20:40:04 -0300

    Update axios.js
    cambio de local

--- front-utn/src/config/axios.js
+++ front-utn/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clienteAxios  = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://utn-back.netlify.app/",
 });
 
 export default clienteAxios
